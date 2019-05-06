23bd114b6c4189cf92629456ddd24ebff560b5e5
Poli <admin@poli.fun>
2019-05-06 11:28:54 +0200

    Update 0.16.51

--- JoinAndPlayCoop-Scenario-0.16.51/OLD/locale/rso/rso_config.lua
+++ JoinAndPlayCoop-Scenario-0.16.51/OLD/locale/rso/rso_config.lua
@@ -8,24 +8,24 @@ use_donut_shapes = false		-- setting this to false will remove donuts from possi
 
 starting_area_size = 1         	-- starting area in regions, safe from random nonsense
 
-absolute_resource_chance = 0.40 -- chance to spawn an resource in a region
+absolute_resource_chance = 50 -- chance to spawn an resource in a region
 starting_richness_mult = 1		-- multiply starting area richness for resources
 global_richness_mult = 1		-- multiply richness for all resources except starting area
-global_size_mult = 1			-- multiply size for all ores, doesn't affect starting area
+global_size_mult = 1.5			-- multiply size for all ores, doesn't affect starting area
 
-absolute_enemy_chance = 3	      -- chance to spawn enemies per sector (can be more then one base if spawned)
+absolute_enemy_chance = 0.5	      -- chance to spawn enemies per sector (can be more then one base if spawned)
 enemy_base_size_multiplier = 1  -- all base sizes will be multiplied by this - larger number means bigger bases
 
-multi_resource_active = false			-- global switch for multi resource chances
-multi_resource_richness_factor = 0.60 	-- any additional resource is multiplied by this value times resources-1
+multi_resource_active = true			-- global switch for multi resource chances
+multi_resource_richness_factor = 1 	-- any additional resource is multiplied by this value times resources-1
 multi_resource_size_factor = 0.90
 multi_resource_chance_diminish = 0.6	-- diminishing effect factor on multi_resource_chance
 
 min_amount=250 					-- default value for minimum amount of resource in single pile
 
-richness_distance_factor= 0.8 	-- exponent for richness distance factor calculation
+richness_distance_factor= 1 	-- exponent for richness distance factor calculation
 fluid_richness_distance_factor = 0.8 -- exponent for richness distance factor calculation for fluids
-size_distance_factor=0.3	   	-- exponent for size distance factor calculation
+size_distance_factor=0.1	   	-- exponent for size distance factor calculation
 
 deterministic = true           	-- set to false to use system for all decisions  math.random
 
